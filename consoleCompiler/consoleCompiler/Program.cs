9a277fc661d943e67cbb5f6680eb93180b4f8e08
Qasmi2 <nadeemqasmi40@yahoo.com>
2016-04-09 18:51:04 +0500

    // Assignment 2 Compiler Command line in C#

---
+++ consoleCompiler/consoleCompiler/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Microsoft.CSharp;
+
+namespace consoleCompiler
+{
+    class Program
+    {
+        public static object Keys { get; private set; }
+        static string mystr = "NULL", compile = "NULL";
+        static char ch;
+
+
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.WriteLine("\t 1 - Press 1 to Write C# code : \n\t 2 - Press 5 to Write compile the code : \n\t    \n\t    Enter your Option : _   ");
+                string str = Console.ReadLine();
+
+                switch (str)
+                {
+                    case "1":
+                        Console.WriteLine("............................................ \n Start Writting a code \n  ");
+                        // start to Writting a Code 
+
+                        mystr = getcode();
+
+                        break;
+
+                    case "5":
+                        // start compile a code 
+
+                        CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+                        ICodeCompiler icc = codeProvider.CreateCompiler();
+                        // string Output = "Out.exe";
+                        System.CodeDom.Compiler.CompilerParameters parameters = new CompilerParameters();
+
+
+                        // send code(written by us) to compilier to check erro 
+                        CompilerResults results = icc.CompileAssemblyFromSource(parameters, mystr);
+
+
+
+                        if (results.Errors.Count > 0)
+                        {
+
+                            foreach (CompilerError CompErr in results.Errors)
+                            {
+                                compile = compile +
+                                              "Line number " + CompErr.Line +
+                                              ", Error Number: " + CompErr.ErrorNumber +
+                                              ", '" + CompErr.ErrorText + ";" +
+                                              Environment.NewLine + Environment.NewLine;
+                                Console.WriteLine(compile);
+                                Console.WriteLine("\n\n ------------------------------------------- ");
+
+                               
+
+                            
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Successful Compile");
+                        }
+                        break;
+
+
+
+
+                    default:
+                        Console.WriteLine("Invalid Entery ");
+                        break;
+                }
+
+            } while (ch !='e');
+        
+            Console.ReadKey();
+
+
+        }
+
+        // Get Code  for compiling 
+        public static string getcode()
+        {
+            string line;
+            
+                line = Console.ReadLine();
+            
+            return line;
+
+        }
+    }
+}
---
+++ consoleCompiler/consoleCompiler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("consoleCompiler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("consoleCompiler")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("38cda7b4-3176-4f1d-ab8e-134c68a940d0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
