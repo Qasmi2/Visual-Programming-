1b0eed15be24e33e3179baba653e95cfed3d9bf1
Qasmi2 <Nadeemqasmi40@yahoo.com>
2016-04-17 23:09:54 +0500

    // RTSP C#

---
+++ vp Project/RTSP/RTSP/RTSPUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rtsp
+{
+    public static class RtspUtils
+    {
+        /// <summary>
+        /// Registers the URI.
+        /// </summary>
+        public static void RegisterUri()
+        {
+            if (!UriParser.IsKnownScheme("rtsp"))
+                UriParser.Register(new HttpStyleUriParser(), "rtsp", 554);
+
+            UriParser.Register(new HttpStyleUriParser(), "rtsp", 8080);
+
+        }
+    }
+}
